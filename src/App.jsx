6b74844161079304bcc9b18df2ccf8f98665da2a
Lester Lee <contact@lester-lee.com>
2024-10-02 14:54:58 -0400

    Clear out starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { useState } from 'react'
-import bookLogo from './assets/books.png'
+import bookLogo from "./assets/books.png";
 
-function App() {
-  const [token, setToken] = useState(null)
-
+export default function App() {
   return (
     <>
-      <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
-
-      <p>Complete the React components needed to allow users to browse a library catalog, check out books, review their account, and return books that they've finished reading.</p>
-
-      <p>You may need to use the `token` in this top-level component in other components that need to know if a user has logged in or not.</p>
-
-      <p>Don't forget to set up React Router to navigate between the different views of your single page application!</p>
+      <h1>Book Buddy</h1>
+      <img src={bookLogo} alt="Book Buddy" />
     </>
-  )
+  );
 }
-
-export default App
